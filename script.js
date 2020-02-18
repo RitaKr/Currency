8a6d2380868a551212f7c4b596ecd6641bb28ac0
RitaKr <ritakra2005@gmail.com>
2020-02-18 19:57:55 +0200

    script almost done

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+/*
 function calc() {
   fetch("items.json")
     .then(res => res.json())
     .then(data => console.log(data));
 }
 calc();
+*/
+const currencyFrom = document.getElementById("currency-one");
+const currencyTo = document.getElementById("currency-two");
+const amountFrom = document.getElementById("amount-one");
+const amountTo = document.getElementById("amount-two");
+const swap = document.getElementById("swap");
+currencyFrom.addEventListener("change", calculate);
+currencyTo.addEventListener("change", calculate);
+amountFrom.addEventListener("input", calculate);
+amountTo.addEventListener("input", calculate);
+function calculate() {
+  //console.log("hello");
+  const currencyFromValue = currencyFrom.value;
+  const currencyToValue = currencyTo.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyFromValue}`)
+    .then(res => res.json())
+    .then(data => {
+      const rateValue = data.rates[currencyToValue].toFixed(2);
+      rate.innerText = `1 ${currencyFromValue} = ${rateValue} ${currencyToValue}`;
+      amountTo.value = (amountFrom.value * rateValue).toFixed(2);
+    });
+}
